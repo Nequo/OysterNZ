ada9c1a755bc6034e4afa4d10dc3f5fe457c30eb
Nequo <nedxtreme@gmail.com>
2017-11-23 18:48:26 +0000

    Added Test class for TravelTracker

--- src/test/JourneyTest.java
+++ src/test/JourneyTest.java
@@ -1,4 +1,10 @@
 package test;
 
+import com.tfl.billing.JourneyStart;
+import org.junit.Test;
+
 public class JourneyTest {
+
+    @Test public void JourneyStartsAtCorrectPlace(){
+    }
 }
---
+++ src/test/TravelTrackerTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import com.tfl.billing.TravelTracker;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+
+
+public class TravelTrackerTest {
+    TravelTracker travelTracker = new TravelTracker();
+
+    @Test
+    
+}
